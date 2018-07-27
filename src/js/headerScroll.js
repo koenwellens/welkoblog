49e60c5e7e0cc3fcbab39b665a1faeadd77a71e1
koenwellens <koen.wellens@hotmail.com>
2018-07-27 21:30:48 +0200

    Make header scroll away less fast

--- src/js/headerScroll.js
+++ src/js/headerScroll.js
@@ -1,40 +1,54 @@
 (function () {
 // Hide Header on on scroll down
     var didScroll;
+    var st;
+    var isOverThreshold;
     var lastScrollTop = 0;
     var delta = 5;
     var navbarHeight = $('.bar-header').outerHeight();
 
-    $(window).scroll(function (event) {
+    $(window).scroll(function(event){
         didScroll = true;
+        st = $(this).scrollTop();
+        if(st <= navbarHeight){
+            isOverThreshold = false;
+        }else{
+            isOverThreshold = true;
+        }
     });
 
-    setInterval(function () {
+    setInterval(function() {
         if (didScroll) {
             hasScrolled();
             didScroll = false;
         }
-    }, 250);
+    }, 150);
 
     function hasScrolled() {
-        var st = $(this).scrollTop();
-
         // Make sure they scroll more than delta
-        if (Math.abs(lastScrollTop - st) <= delta)
+        if(Math.abs(lastScrollTop - st) <= delta)
             return;
 
         // If they scrolled down and are past the navbar, add class .nav-up.
         // This is necessary so you never see what is "behind" the navbar.
-        if (st > lastScrollTop && st > navbarHeight){
+        if (st > lastScrollTop && st > navbarHeight && isOverThreshold){
             // Scroll Down
-            $('.bar-header').removeClass('nav-down').addClass('nav-up');
+            hideMenuBar();
         } else {
             // Scroll Up
             if(st + $(window).height() < $(document).height()) {
-                $('.bar-header').removeClass('nav-up').addClass('nav-down');
+                showMenuBar();
             }
         }
 
         lastScrollTop = st;
+    }
+
+    function hideMenuBar(){
+        $('.bar-header').animate({top:'-70px'}, 50, 'swing');
+    }
+
+    function showMenuBar(){
+        $('.bar-header').animate({top:'0px'}, 10, 'swing');
     }
 })();
